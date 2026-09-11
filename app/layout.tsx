e51e4f13f71de83c9b14d871e5d85ba3d8b641e1
adhyadixit1 <ojasvy33@gmail.com>
2026-09-11 13:27:41 +0530

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import {Suspense} from 'react';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -32,9 +33,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PGTKV6S');`}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5PGTKV6S"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Header />
         <JsonLd data={{'@context':'https://schema.org','@type':'Organization',name:'Paper & Press',url:siteUrl,logo:siteUrl+'/brand/brand-mark.png',telephone:'+918824622541',address:{'@type':'PostalAddress',addressLocality:'Jaipur',addressRegion:'Rajasthan',addressCountry:'IN'}}}/>
         <Suspense fallback={null}><VisitorAnalytics/></Suspense>
